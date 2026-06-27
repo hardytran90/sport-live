e753b530d5fc054ded7860fcb482a80510d5ea9b
hardytran90 <bavinhhung.tran@student.adelaide.edu.au>
2026-06-27 23:15:06 +0930

    added live page + standing page

--- index.js
+++ index.js
@@ -13,38 +13,86 @@ app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
     try {
-    const result = await axios.get(`https://api.the-odds-api.com/v4/sports/?apiKey=${API_KEY}`);
-    const soccerSports = result.data.filter(item => item.group === "Soccer");
-    res.render("index.ejs", {sports: soccerSports});
+    const today = new Date().toISOString().split("T")[0];
+
+    const result = await axios.get("https://v3.football.api-sports.io/fixtures", {
+        params: { league: 39, season: 2024, date: today, },
+        headers: { "x-apisports-key": process.env.API_KEY },
+    });
+
+    const fixtures = result.data.response;
+    console.log(JSON.stringify(fixtures[0], null, 2), );
+    res.render("index.ejs", { fixtures, page: "home" });
+
     } catch (error) {
-        console.log(error.response.data);
+        console.log(error.response?.data);
         res.status(500).send("Failed to load sport list!")
     }
 });
 
-app.post("/sports", async (req, res) => {
+app.get("/standings", async (req, res) => {
     try {
-    const sportKey = req.body.sport;
-    const [sportsResult, scoresResult] = await Promise.all([
-        axios.get(`https://api.the-odds-api.com/v4/sports/?apiKey=${API_KEY}`),
-        axios.get(`https://api.the-odds-api.com/v4/sports/${sportKey}/scores/?apiKey=${API_KEY}&daysFrom=3`),
-    ]);
-    const soccerSports = sportsResult.data.filter(item => item.group === "Soccer");
-    const games = scoresResult.data
-    .filter(game => game.completed === true || game.scores !== null)
-    .sort((a,b) => a.completed - b.completed);
-    
-    res.render("index.ejs", {
-        sports: soccerSports,
-        games: games,
-    });
+        const result = await axios.get("https://v3.football.api-sports.io/standings", {
+            params: { league: 39, season: 2024 },
+            headers: { "x-apisports-key": process.env.API_KEY },
+        });
+        const leagueData = result.data.response[0].league;
+        const teams = leagueData.standings[0];
+        res.render("standings.ejs", { 
+            leagueName: leagueData.name,
+            leagueLogo: leagueData.logo,
+            season: leagueData.season,
+            teams: teams,
+         });
+    } catch (error) {
+        console.log(error.response?.data);
+        res.status(500).send("Failed to load standings!");
+    }
+});
+
+app.get("/live", async (req, res) => {
+    try {
+        const result = await axios.get("https://v3.football.api-sports.io/fixtures", {
+            params: { live: "all" },
+            headers: { "x-apisports-key": process.env.API_KEY },
+        })
+        console.log(result.data.response);
+        
+        const fixtures = result.data.response || [];
+        // Group as league
+        const grouped = {};
+        fixtures.forEach(fix => {
+            const leagueId = fix.league.id;
+            if (!grouped[leagueId]) {
+                grouped[leagueId] = {
+                    league: fix.league,
+                    games: []
+                };
+            }
+            grouped[leagueId].games.push(fix);
+        });
+
+        const groupedFixtures = Object.values(grouped);
+        res.render("live.ejs", { groupedFixtures, page: "live" });
     
     } catch (error) {
-        console.log(error.response.data);
-        res.status(404).send("Data not found!")
+        res.status(500).send("Failed to load live matches!");
+    }
+});
+
+app.get("/events/:id", async (req, res) => {
+    try {
+        const result = await axios.get("https://v3.football.api-sports.io/fixtures/events", {
+            params: { fixture: req.params.id },
+            headers: { "x-apisports-key": process.env.API_KEY },
+        });
+        res.json(result.data.response);
+    } catch (error) {
+        console.log(error.response?.data);
+        res.status(500).json({ error: "Failed to load events" });
     }
 });
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
-})
+});
